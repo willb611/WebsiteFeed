40fed5043102360dbc17efc0b694cb3f5d6ae60f
William A. O. Brown <will.b611@gmail.com>
2016-12-03 14:03:13 +0000

    Fix test message

--- FeedWebpage/__tests__/PostListTest.jsx
+++ FeedWebpage/__tests__/PostListTest.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import PostList from '../Scripts/PostList.jsx';
 
-describe('Welcome (Snapshot)', () => {
-    it('Welcome renders PostList', () => {
+describe('PostList (Snapshot)', () => {
+    it('PostList renders PostList', () => {
         const someData = [];
         const component = renderer.create(<PostList data={someData}/>);
         const json = component.toJSON();
